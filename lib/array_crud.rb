6eec96bc13c8cc6b5f6142c055aca5163e03f969
Alfred Wang <alfredwang0707@gmail.com>
2020-09-30 17:35:08 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,39 +1,39 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  [cat, dog, bird, fish]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  add_element_to_end_of_array.push(frog)
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  add_element_to_start_of_array.unshift(sheep)
 end
 
 def remove_element_from_end_of_array(array)
-  
+  remove_element_from_end_of_array.pop()
 end
 
 def remove_element_from_start_of_array(array)
-  
+  remove_element_from_start_of_array.shift()
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  retrieve_element_from_index[2]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  retrieve_first_element_from_array.index(0)
 end
 
 def retrieve_last_element_from_array(array)
-  
+  retrieve_last_element_from_array.index(-1)
 end
 
-def update_element_from_index(array, index_number, element)
+def update_element_from_index(array, index_number, elemen)
 
 end
